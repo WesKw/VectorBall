eaf3588656f72cf21595b403b872456bf170af8e
WesKw <kwiecinskiwesley@gmail.com>
2021-05-11 20:58:37 -0500

    Title screen uses focus for buttons

--- src/Title/Title.cs
+++ src/Title/Title.cs
@@ -3,66 +3,27 @@ using System;
 
 public class Title : Control
 {
-	private enum Btn
-	{
-		Start,
-		Exit
-	}
-	
-	private Btn currentBtn;
 	private VBoxContainer btnContainer;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		currentBtn = Btn.Start;
 		btnContainer = GetNode("MarginContainer/Buttons/VBoxContainer") as VBoxContainer;
-		btnContainer.GetChild<Button>((int)currentBtn).Modulate = new Color(1.0f, 1.0f, 1.0f, 1.0f);
-		btnContainer.GetChild<Button>(1).Modulate = new Color(1.0f, 1.0f, 1.0f, 0.5f);
 		GetNode<AnimationPlayer>("AnimationPlayer").Play("FadeIn");
 		//GetNode<AudioStreamPlayer>("AudioStreamPlayer").Play(0);
-	}
-	
-	public override void _Process(float delta)
-	{
-		if(Input.IsActionJustPressed("ui_accept"))
-		{
-			btnContainer.GetChild<Button>((int)currentBtn).EmitSignal("pressed");
-		}
+		btnContainer.GetChild<Button>(0).GrabFocus();
 	}
 	
 	//Handles switching menu options
 	public override void _UnhandledInput(InputEvent @event)
 	{
-		Btn previousBtn = currentBtn;
-		
-		if(Input.IsActionJustPressed("ui_up"))
-		{
-			currentBtn--;
-			if((int)currentBtn < 0)
-				currentBtn = Btn.Exit;
-		} else if(Input.IsActionJustPressed("ui_down"))
-		{
-			currentBtn++;
-			if((int)currentBtn > 1)
-				currentBtn = Btn.Start;
-		}
-		
-		if(Input.IsKeyPressed(70))
-			OS.WindowFullscreen = !OS.WindowFullscreen;
-		
-		btnContainer.GetChild<Button>((int)previousBtn).Modulate = new Color(1.0f, 1.0f, 1.0f, 0.5f);
-		btnContainer.GetChild<Button>((int)currentBtn).Modulate = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+		//if(Input.IsActionJustPressed("toggle_fullscreen"));
+			//OS.WindowFullscreen = !OS.WindowFullscreen;
 	}
 	
-	private void _on_Start_pressed()
-	{
-		GetTree().ChangeScene("res://scenes/Main.tscn");
-	}
-	
-	private void _on_Exit_pressed()
-	{
-		GetTree().Quit();
-	}
-	
+	#region ButtonPresses
+	private void _on_Start_pressed() => GetTree().ChangeScene("res://scenes/Main.tscn");
+	private void _on_LevelSelect_pressed() => GD.Print("Level Select");
+	private void _on_Exit_pressed() => GetTree().Quit();
+	#endregion
 }
